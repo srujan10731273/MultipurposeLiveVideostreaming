d7896ca9845f545ee632a9f304c31240ffe0c8ba
srujan10731273 <152582379+srujan10731273@users.noreply.github.com>
2023-12-05 09:53:36 +0530

    Update webCam_color_resolution.c

--- webCam_color_resolution.c
+++ webCam_color_resolution.c
@@ -12,6 +12,27 @@
 sem_t semaphore;
 // Allocate a buffer to store one frame
 unsigned char frame[H][W][3] = {0};
+void* colorInvert();
+void* resolution();
+void* image_stabilization();
+//main function
+int main()
+{
+   
+   sem_init(&semaphore,0,1);
+  pthread_t thread_id1,thread_id2,thread_id3;
+  //thread creation for color inversion
+  pthread_create(&thread_id1,NULL,colorInvert,NULL);
+  pthread_join(thread_id1,NULL);
+   //thread creation for changing the resolution
+  pthread_create(&thread_id2,NULL,resolution,NULL);
+  pthread_join(thread_id2,NULL);
+  //creation of thread for image stabilization
+  pthread_create(&thread_id3,NULL,image_stabilization,NULL);
+  pthread_join(thread_id3,NULL);
+    return 0;
+}
+//color Invert
 void* colorInvert(){
     uint16_t x, y;
     uint32_t count;
@@ -106,20 +127,4 @@ void* resolution(){
 //Image Stabilization
 void* image_stabilization(){
       printf("Image stabilization is not possible\n");
-}
-int main()
-{
-   
-   sem_init(&semaphore,0,1);
-  pthread_t thread_id1,thread_id2,thread_id3;
-  //thread creation for color inversion
-  pthread_create(&thread_id1,NULL,colorInvert,NULL);
-  pthread_join(thread_id1,NULL);
-   //thread creation for changing the resolution
-  pthread_create(&thread_id2,NULL,resolution,NULL);
-  pthread_join(thread_id2,NULL);
-  //creation of thread for image stabilization
-  pthread_create(&thread_id3,NULL,image_stabilization,NULL);
-  pthread_join(thread_id3,NULL);
-    return 0;
 }
