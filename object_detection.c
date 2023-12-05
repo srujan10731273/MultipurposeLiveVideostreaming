7efa70822c34021b8ed3ef8bbf0d5791d1f468d6
srujan10731273 <152582379+srujan10731273@users.noreply.github.com>
2023-12-05 10:22:19 +0530

    Update object_detection.c

--- object_detection.c
+++ object_detection.c
@@ -8,15 +8,15 @@ int main() {
     return 1;
   }
 
-  SDL_Window *window =
-      SDL_CreateWindow("Object Detection Example", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+  //creation of window to display frames
+  SDL_Window *window = SDL_CreateWindow("Object Detection Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
   if (window == NULL) {
     fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
     SDL_Quit();
     return 1;
   }
 
+  //to generate images
   SDL_Renderer *renderer =
       SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   if (renderer == NULL) {
@@ -26,6 +26,7 @@ int main() {
     return 1;
   }
 
+  //loading images to the sdl_surface using img_load
   SDL_Surface *imageSurface = IMG_Load("out_frame.jpg");
   //printf("object detected");
   if (imageSurface == NULL) {
@@ -36,8 +37,8 @@ int main() {
     return 1;
   }
 
-  SDL_Texture *imageTexture =
-      SDL_CreateTextureFromSurface(renderer, imageSurface);
+  //converting image data to format suitable for GPU
+  SDL_Texture *imageTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
   if (imageTexture == NULL) {
     fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
     SDL_FreeSurface(imageSurface);
@@ -47,13 +48,17 @@ int main() {
     return 1;
   }
 
+  //defining destRect variable 
   SDL_Rect destRect = {0, 0, imageSurface->w, imageSurface->h};
 
+  //clearing renderer
   SDL_RenderClear(renderer);
+
+  //copying converted data to renderer 
   SDL_RenderCopy(renderer, imageTexture, NULL, &destRect);
   
   
-  
+  //for object detection based on RGB intensity values
   Uint32 *pixels = (Uint32 *)imageSurface->pixels;
     int objectFound = 0;
 
@@ -75,10 +80,11 @@ int main() {
     }
 
   
-  
+  //for displaying final generated image
   SDL_RenderPresent(renderer);
 
-  SDL_Delay(3000); // Show the image for 3 seconds
+  // Show the image for 3 seconds
+  SDL_Delay(3000); 
 
   SDL_DestroyTexture(imageTexture);
   SDL_FreeSurface(imageSurface);
