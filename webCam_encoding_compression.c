1b82ed30dbb3e436503b9d40c840e12b79f9cb14
srujan10731273 <152582379+srujan10731273@users.noreply.github.com>
2023-12-05 09:44:00 +0530

    Update webCam_encoding_compression.c

--- webCam_encoding_compression.c
+++ webCam_encoding_compression.c
@@ -57,7 +57,7 @@ void *compressVideo(void *arg)
 {
     // Wait for the encoding process to finish
      sem_wait(&semaphore);
-     system("ffmpeg -i webCam_encoded.mp4 -c:v libx265 -crf 28 -c:a aac -strict experimental -b:a 128k webCam_compressed.mp4");
+     system("ffmpeg -i webCam_encoded.mp4 -c:v libx265 -crf 28 webCam_compressed.mp4");
      printf("Webcam encoded frames are compressed\n");
      sleep(2);
      system("mplayer webCam_compressed.mp4");
